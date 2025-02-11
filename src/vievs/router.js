23c71fb3577db5c3e25573f5d5788074c0e074dd
Ken Karlson <javarecords@mail.ru>
2025-02-11 18:00:07 +0300

    Fix /#/

--- src/vievs/router.js
+++ src/vievs/router.js
@@ -4,12 +4,13 @@ import Router from 'vue-router';
 Vue.use(Router);
 
 //Pages
-import Home from './pages/Home.vue';
-import About from './pages/About.vue';
-import Example from './pages/Example.vue';
+import Home from '../pages/Home.vue';
+import About from '../pages/About.vue';
+import Example from '../pages/Example.vue';
 
 //Routings
 export default new Router({
+  mode: 'history',
   routes:[
     { path: '/', name: 'home', component: Home },
     { path: '/about', name: 'about', component: About },
